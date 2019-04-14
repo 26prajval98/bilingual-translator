e4f92e4c8873c5a2763cd6587f85a82a22d01f6a
26prajval98 <26prajval98@gmail.com>
2019-04-14 16:26:40 +0530

    Data to sentences

--- use_to_gen.py
+++ use_to_gen.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import copy
 import re
 
 content = open("source.txt", "r", encoding="utf-8").read()
@@ -8,7 +9,29 @@ sp = BeautifulSoup(content, "html.parser")
 
 [s.extract() for s in sp("fs")]
 
-x = "".join(sp.find_all("sentence")[0].findAll(text=True))
-x = re.sub(r"\t", "", x)
-x = re.sub(r"[A-Za-z]|_|\)\)\n|\(\(", "", x)
-print(x)
+sentences = []
+
+for s in sp.find_all("sentence"):
+    x = "".join(s.findAll(text=True))
+    x = re.sub(r"\t", "", x)
+    x = re.sub(r"[A-Za-z]|_|\)\)\n|\(\(|[0-9]+\.[0-9]+", "", x)
+    x = re.split(r"[0-9]+\n", x)
+    for i in range(len(x)):
+        x[i] = re.split("\n", x[i])[0:-1]
+
+    temp_sentences = [""]
+    for i in range(len(x)):
+        t = x[i]
+        if t[0] == ".":
+            break
+        temp_sentences2 = copy.deepcopy(temp_sentences)
+        temp_sentences = []
+        for j in range(len(t)):
+            new_word = t[j]
+            for k in range(len(temp_sentences2)):
+                if new_word != "":
+                    ns = temp_sentences2[k] + new_word + " "
+                else:
+                    ns = temp_sentences2[k]
+                temp_sentences.append(ns)
+    sentences = [*sentences, *temp_sentences]
